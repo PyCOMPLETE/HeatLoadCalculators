e0ff6b403cca03f3b8f92f2f507d01208f65376b
giadarol <giovanni.iadarola@cern.ch>
2018-04-06 11:28:43 +0200

    Status

--- FillCalculator.py
+++ FillCalculator.py
@@ -9,16 +9,16 @@ from LHCMeasurementTools.LHC_Energy import energy
 
 class HeatLoad_calculated_fill(object):
     def __init__(self, fill_dict, heat_load_calculator, Dt_calc = 60., fbct_dict=None, bct_dict=None, blength_dict=None):
- 
+
         """
         Returns the half cell heat load for a fill dict, which has to consist of basic and bunchbybunch data
         """
-        
-        
+
+
         self.heat_load_calculator = heat_load_calculator
- 
+
         ene = energy(fill_dict, beam=1)
- 
+
         # Different for both beams
         self.heat_load_calculated_per_beam_Wm = {}
         for beam_ctr in (1,2):
@@ -38,14 +38,14 @@ class HeatLoad_calculated_fill(object):
             else:
                 bunch_length = blength_dict[beam_ctr]
 
-            
+
             if beam_ctr == 1:
                 self.t_stamps = np.arange(bct.t_stamps[0], bct.t_stamps[-1], Dt_calc)
-                
+
             ppb = []
             energy_eV = []
             sigma_t = []
-            
+
             for tt in self.t_stamps:
                 fbct_trace = fbct.nearest_older_sample(tt)
 
@@ -53,22 +53,22 @@ class HeatLoad_calculated_fill(object):
                 if summed != 0.:
                     fbct_trace *= bct.nearest_older_sample(tt)/summed
                 bl_trace = bunch_length.nearest_older_sample(tt)
-                
+
                 mask_no_bunch = bl_trace<1e-15
                 bl_trace[mask_no_bunch] = 1.
                 fbct_trace[mask_no_bunch] = 0.
-    
+
                 ppb.append(fbct_trace)
                 sigma_t.append(bl_trace/4.)
                 energy_eV.append(ene.nearest_older_sample(tt)*1e9)
-                
-                
-                
+
+
+
             ppb = np.array(ppb)
             sigma_t = np.array(sigma_t)
             energy_eV = np.array(energy_eV)
-            
+
             self.heat_load_calculated_per_beam_Wm['beam_%d'%beam_ctr] = self.heat_load_calculator.calculate_P_Wm(ppb, sigma_t, energy_eV=energy_eV)
-        
+
         self.heat_load_calculated_total = self.heat_load_calculated_per_beam_Wm['beam_1'] + self.heat_load_calculated_per_beam_Wm['beam_2']
-               
+
--- impedance_heatload.py
+++ impedance_heatload.py
@@ -9,7 +9,7 @@ from scipy.special import gamma as gammafunc
 default_rho_vs_T_file =  os.path.dirname(os.path.realpath(__file__)) + '/copper_rho_vs_T.txt'
 
 lhc_circumference = 26658.883
-lhc_arc_chamb_radius = 18.4e-3 
+lhc_arc_chamb_radius = 18.4e-3
 lhc_arc_weld_thickness = 2e-3
 rho_stainless_steel = 6e-7 # Elias
 
@@ -24,28 +24,28 @@ class HeatLoadCalculatorImpedance(object):
         if type(rho_vs_T) is str:
             data = np.loadtxt(rho_vs_T)
         else:
-            data = rho_vs_T  
+            data = rho_vs_T
         self.copper_rho_Ohm_m = interp1d(data[:,0], data[:,1]*1e-8)
-        
+
         self.temperature_K = temperature_K
         self.circumference_m = circumference_m
         self.chamb_radius_m = chamb_radius_m
         self.weld_Thickness_m = weld_Thickness_m
         self.weld_Rho_Ohm_m = weld_Rho_Ohm_m
-        
-        
+
+
     def calculate_P_Wm(self, ppb, sigma_t, b_field=0., n_bunches=None):
-    
-            
+
+
         """
-        If n_bunches is not specified, ppb has to be either a numpy array with dimensions 
+        If n_bunches is not specified, ppb has to be either a numpy array with dimensions
         time_steps x n_bunches or n_bunches.
         If n_bunches is specified, ppb can be either a number or an arry with dimensions of timesteps
         sigma_t is either a float or a np array of dimensions time_steps or timp_steps * n_bunches
         Do not specify n_bunches explicitly and implicitly at the same time!
         """
-        
-        
+
+
         if n_bunches is None:
             if not hasattr(ppb, '__iter__'):
                 raise ValueError('N_bunches has to be specified somehow!')
@@ -57,21 +57,21 @@ class HeatLoadCalculatorImpedance(object):
 
 
         rho_B0_T = self.copper_rho_Ohm_m(self.temperature_K) # 0.014 *1e-8 for 20 K
-        
+
         if not hasattr(b_field, '__iter__') and b_field == 0.:
             rho_B_T = rho_B0_T
         else:
             rho_B0_273 = self.copper_rho_Ohm_m(273.)
             #elias
             rho_B_T = rho_B_T = rho_B0_T*(1 + 10**(1.055*np.log10(b_field*(rho_B0_273/rho_B0_T))- 2.69))
-           
+
             #philipp
             #try:
             #   rho_B0_4 = self.copper_rho_Ohm_m(4.)
             #except ValueError:
-            #    rho_B0_4 = 3.*self.copper_rho_Ohm_m(4.2) - 2.*self.copper_rho_Ohm_m(4.3)            
+            #    rho_B0_4 = 3.*self.copper_rho_Ohm_m(4.2) - 2.*self.copper_rho_Ohm_m(4.3)
             #rho_B_T = rho_B0_T * (1. + 10.**(-2.69) * (b_field*rho_B0_273/rho_B0_4)**1.055)
-            
+
             #benoit/nicolas
             #rho_B_T = rho_B0_T *(1.0048 + 0.0038*b_field*self.copper_rho_Ohm_m(300.)/self.copper_rho_Ohm_m(20.))
             #For 20K!!!
@@ -92,37 +92,37 @@ class HeatLoadCalculatorImpedance(object):
             weld_Factor = 1.
 
         return P_no_weld*weld_Factor
-        
 
-                
+
+
 def B_lhc_arc_dipole(energy_eV):
     return 8.33*energy_eV/7e12
 def grad_lhc_arc_quad(energy_eV):
     return 200.*energy_eV/7e12
-                
+
 
 class HeatLoadCalculatorImpedanceLHCArc(object):
-    
+
     def __init__(self, lhc_arc_bs_temperature = 20.):
-        self.hl_beam_screen = HeatLoadCalculatorImpedance(temperature_K = lhc_arc_bs_temperature, 
+        self.hl_beam_screen = HeatLoadCalculatorImpedance(temperature_K = lhc_arc_bs_temperature,
             circumference_m=lhc_circumference, chamb_radius_m=lhc_arc_chamb_radius,
             weld_Thickness_m=lhc_arc_weld_thickness, weld_Rho_Ohm_m=rho_stainless_steel)
-    
+
     def calculate_P_Wm_dipole(self, ppb, sigma_t, energy_eV=0., n_bunches=None):
         return self.hl_beam_screen.calculate_P_Wm(ppb, sigma_t, b_field=B_lhc_arc_dipole(energy_eV), n_bunches=n_bunches)
-        
+
     def calculate_P_Wm_quad(self, ppb, sigma_t, energy_eV=0., n_bunches=None):
-        return self.hl_beam_screen.calculate_P_Wm(ppb, sigma_t, 
+        return self.hl_beam_screen.calculate_P_Wm(ppb, sigma_t,
             b_field=grad_lhc_arc_quad(energy_eV)*self.hl_beam_screen.chamb_radius_m, n_bunches=n_bunches)
-    
+
     def calculate_P_Wm_drift(self, ppb, sigma_t, energy_eV=0., n_bunches=None):
-        return self.hl_beam_screen.calculate_P_Wm(ppb, sigma_t, 
+        return self.hl_beam_screen.calculate_P_Wm(ppb, sigma_t,
             b_field=0., n_bunches=n_bunches)
-            
+
     def calculate_P_Wm(self, ppb, sigma_t, energy_eV=0., n_bunches=None):
         #return self.calculate_P_Wm_dipole(ppb, sigma_t, energy_eV, n_bunches)#benoit assumption
          return (self.calculate_P_Wm_dipole(ppb, sigma_t, energy_eV, n_bunches)*14.3*3+\
                  self.calculate_P_Wm_quad(ppb, sigma_t, energy_eV, n_bunches)*3.1+\
                  self.calculate_P_Wm_drift(ppb, sigma_t, energy_eV, n_bunches)*7.)/53.
-                        
-  
+
+
