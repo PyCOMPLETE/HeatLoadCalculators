4fbb096cd7c3eda13f871ec9a9551060afa18cb7
giadarol <giovanni.iadarola@cern.ch>
2020-08-28 13:47:17 +0200

    python 3 migration

--- 000_test_single_point.py
+++ 000_test_single_point.py
@@ -5,7 +5,7 @@ sys.path.append(BIN)
 
 
 import impedance_heatload as ihl
-import synchrotron_radiation_heatload as srhl 
+import synchrotron_radiation_heatload as srhl
 
 
 ppb_test = 1.15e11
@@ -18,14 +18,14 @@ hlsr_calculator  = srhl.HeatLoadCalculatorSynchrotronRadiationLHCArc()
 
 
 hl_imped_dip = hli_calculator.calculate_P_Wm_dipole(ppb=ppb_test, sigma_t=sigma_t_test,  energy_eV=energy_eV_test, n_bunches=n_bunches_test)
-print 'Impedance load in the dipoles: %.1f mW/m/beam'%(hl_imped_dip*1000)
+print('Impedance load in the dipoles: %.1f mW/m/beam'%(hl_imped_dip*1000))
 hl_imped_quad = hli_calculator.calculate_P_Wm_quad(ppb=ppb_test, sigma_t=sigma_t_test,  energy_eV=energy_eV_test, n_bunches=n_bunches_test)  
-print 'Impedance load in the quadrupoles: %.1f mW/m/beam'%(hl_imped_quad*1000)
+print('Impedance load in the quadrupoles: %.1f mW/m/beam'%(hl_imped_quad*1000))
 hl_imped_drift = hli_calculator.calculate_P_Wm_drift(ppb=ppb_test, sigma_t=sigma_t_test,  energy_eV=energy_eV_test, n_bunches=n_bunches_test)
-print 'Impedance load in the drifts: %.1f mW/m/beam'%(hl_imped_drift*1000)
+print('Impedance load in the drifts: %.1f mW/m/beam'%(hl_imped_drift*1000))
 hl_imped_hcell = hli_calculator.calculate_P_Wm(ppb=ppb_test, sigma_t=sigma_t_test,  energy_eV=energy_eV_test, n_bunches=n_bunches_test)  
-print 'Impedance load average half-cell: %.1f mW/m/beam'%(hl_imped_hcell*1000)
+print('Impedance load average half-cell: %.1f mW/m/beam'%(hl_imped_hcell*1000))
 
 
 hl_imped_sr = hlsr_calculator.calculate_P_Wm(ppb=ppb_test, sigma_t=sigma_t_test,  energy_eV=energy_eV_test, n_bunches=n_bunches_test)
-print 'Synchrotron radiation load average half-cell: %.1f mW/m/beam'%(hl_imped_sr*1000)
+print('Synchrotron radiation load average half-cell: %.1f mW/m/beam'%(hl_imped_sr*1000))
